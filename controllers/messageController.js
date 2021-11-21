6837cd376315cb111cf88d86aeff7526238c25d1
Mert18 <mertuygur02@gmail.com>
2021-11-21 23:48:52 +0300

    fed up

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -12,46 +12,19 @@ export const getMessages = async (_, res) => {
 export const createMessage = async (req, res) => {
   const { body, method } = req;
 
-  const { title, email, message, captcha } = body;
+  const { title, email, message } = body;
 
   if (method === "POST") {
-    if (!email || !captcha) {
-      return res.status(422).json({
-        message: "Unproccesable request, please provide the required fields",
-      });
-    }
-
     try {
-      const response = await fetch(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-          },
-          method: "POST",
-        }
-      );
-
-      const captchaValidation = await response.json();
-
-      if (captchaValidation.success) {
-        // Replace this with the API that will save the data received
-        // to your backend
-        // Return 200 if everything is successful
-        const mymessage = new Message({
-          title: title,
-          email: email,
-          message: message,
-        });
-        await mymessage.save();
-        return res.status(201).send({
-          message: "Message created successfully.",
-          createMessage: mymessage,
-        });
-      }
-
-      return res.status(422).json({
-        message: "Unproccesable request, Invalid captcha code",
+      const mymessage = new Message({
+        title: title,
+        email: email,
+        message: message,
+      });
+      await mymessage.save();
+      return res.status(201).send({
+        message: "Message created successfully.",
+        createMessage: mymessage,
       });
     } catch (err) {
       console.log("Error creating message.", err);
