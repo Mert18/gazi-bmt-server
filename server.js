b5838701fff4e7fdb9432acfe97ced9d49778bfb
Mert18 <mertuygur02@gmail.com>
2021-11-22 01:20:18 +0300

    allow cors

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.get("/", (_, res) => {
   res.send("API is running!");
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/api/events", eventRoutes);
 app.use("/api/admins", adminRoutes);
 app.use("/api/messages", messageRoutes);
