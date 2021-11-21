5d07c4b7d50a5306c3ab72630d2a1f016d595b12
Mert18 <mertuygur02@gmail.com>
2021-11-22 00:28:02 +0300

    yan masamda tarihin en yalancı gazetesi

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -10,20 +10,42 @@ export const getMessages = async (_, res) => {
 };
 
 export const createMessage = async (req, res) => {
-  const { title, email, message } = req.body;
+  const { title, email, message, captcha } = req.body;
 
-  try {
-    const mymessage = new Message({
-      title: title,
-      email: email,
-      message: message,
-    });
-    await mymessage.save();
-    return res.status(201).send({
-      message: "Message created successfully.",
-      createMessage: mymessage,
-    });
-  } catch (err) {
-    console.log("Error creating message.", err);
+  if (method === "POST") {
+    if (!email || !captcha || !title || !message) {
+      return res.status(422).json({
+        message: "Unproccesable request, please provide the required fields",
+      });
+    }
+    try {
+      const response = await fetch(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+          },
+          method: "POST",
+        }
+      );
+      const captchaValidation = await response.json();
+
+      if (captchaValidation.success) {
+        const mymessage = new Message({
+          title: title,
+          email: email,
+          message: message,
+        });
+        await mymessage.save();
+        return res.status(200).send("OK");
+      }
+      return res.status(422).json({
+        message: "Unproccesable request, Invalid captcha code",
+      });
+    } catch (err) {
+      console.log(error);
+      return res.status(422).json({ message: "Something went wrong" });
+    }
   }
+  return res.status(404).send("Not found");
 };
